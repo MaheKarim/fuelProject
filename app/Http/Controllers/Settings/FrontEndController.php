75ed3c3e36c662a07731b27703110b9e9021e385
Mahe Karim <mahekarim@gmail.com>
2022-09-11 09:09:58 +0600

    FAQ Service

--- app/Http/Controllers/Settings/FrontEndController.php
+++ app/Http/Controllers/Settings/FrontEndController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\FAQ;
 
 class FrontEndController extends Controller
 {
     public function index()
     {
-        return view('frontend.index');
+        $faqs = FAQ::all();
+        return view('frontend.index', compact('faqs'));
     }
 }
